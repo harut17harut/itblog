2415a3fe61b1dffe902e641bcfb14d73cc52ed45
harut17harut <harut15harut@gmail.com>
2021-07-09 17:36:42 +0400

    packages are required

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const path = require('path');
