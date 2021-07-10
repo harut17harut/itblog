fecd0c01a76668b0b7029e2b0379f48269d8efcb
harut17harut <harut15harut@gmail.com>
2021-07-10 10:17:09 +0400

    user registration module created

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/db');
+
+//user data scheme
+const UserSchema = mongoose.Schema({
+name:{
+    type:String
+},
+email:{
+type:String,
+required:true
+},
+login:{
+    type:String,
+    required:true
+    },
+password:{
+    type:String,
+    required:true
+}
+});
+const User = mudule.exports = mongoose.model('User',UserSchema);
+//get user by login
+module.exports.getUserByLogin = function(login,callback){
+    const query = {login:login};
+    User.findOne(query,callback);
+};
+module.exports.getUserById = function(id,callback){
+    User.findById(id,callback);
+};
+module.exports.addUser = function(newUser,callback){
+    newUser.password
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(newUser.password,salt,(err,hash)=>{
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+   
+   
+};
--- routes/account.js
+++ routes/account.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
-router.get("/reg",(req,res)=>{
-    res.send("registration page");
+const User = require("../models/user");
+// router.get("/reg",(req,res)=>{
+//     res.send("registration page");
+// });
+router.post('/reg',(req,res)=>{
+var newUser = new User({
+    name:req.body.name,
+    email:req.body.email,
+    login:req.body.login,
+    password:req.body.password
 });
-
+User.addUser(newuser,(err,user)=>{
+    if(err)
+    res.json({success:false,msg:"user wasnt registered"});
+    else
+    res.json({success:true,msg:"youve registered succesfully"});
+});
+});
 router.get("/auth",(req,res)=>{
     res.send("authoristion page");
 });
